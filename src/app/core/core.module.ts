362c5efc5fce94588ecd4b8b06550773b14ae1fe
huangqing <2316694914@qq.com>
2021-12-16 10:46:13 +0800

    chore: update ng version

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { StoreModule } from "@ngrx/store";
 
-import { AppConfig } from "../../environments/environment";
-import { reducers, metaReducers, selectFilesState } from "./core.state";
-
-export * from "./files/files";
-
-export { selectFilesState };
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    AppConfig.production
-      ? []
-      : StoreDevtoolsModule.instrument({
-        name: "Angular NgRx Material Starter",
-      }),
   ],
 })
 export class CoreModule {}
